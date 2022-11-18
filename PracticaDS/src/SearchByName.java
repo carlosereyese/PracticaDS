2cdc42fff8e1e83e49545b303f89941587886d80
1601501 <1601501@uab.cat>
2022-11-18 13:29:29 +0100

    Versión 11: CheckStyle aplicado en la clase SearchByName

--- PracticaDS/src/SearchByName.java
+++ PracticaDS/src/SearchByName.java
@@ -1,46 +1,43 @@
 /*
-The "SearchByName" class is used to search for an activity by name, this class implements the "Visitor".
+The "SearchByName" class is used to search for an activity by name,
+this class implements the "Visitor".
 */
-public class SearchByName implements Visitor{
-    private final Activity root;
-    private Activity foundActivity;
-    private String name;
+public class SearchByName implements Visitor {
+  private final Activity root;
+  private Activity foundActivity;
+  private String name;
 
-    public SearchByName(Activity activity)
-    {
-        root = activity;
-    }
+  public SearchByName(Activity activity) {
+    root = activity;
+  }
 
-    @Override
-    public void visitProject(Project project) {
-        if (project.getNameActivity().equals(name))
-        {
-            foundActivity = project;
-        }
-        else
-        {
-            for (int i = 0; i < project.getSizeList(); i ++)
-            {
-                Object o = project.getElementFromList(i);
-                ((Activity) o).acceptVisitor(this);
-            }
-        }
+  @Override
+  public void visitProject(Project project) {
+    if (project.getNameActivity().equals(name)) {
+      foundActivity = project;
+    } else {
+      for (int i = 0; i < project.getSizeList(); i++) {
+        Object o = project.getElementFromList(i);
+        ((Activity) o).acceptVisitor(this);
+      }
     }
-    @Override
-    public void visitTask(Task task) {
-        if (task.getNameActivity().equals(name))
-        {
-            foundActivity = task;
-        }
+  }
+
+  @Override
+  public void visitTask(Task task) {
+    if (task.getNameActivity().equals(name)) {
+      foundActivity = task;
     }
-    @Override
-    public void visitInterval(Interval interval) {
+  }
+
+  @Override
+  public void visitInterval(Interval interval) {
         //This method is not implemented because the intervals have no name.
-    }
+  }
 
-    public Activity searchByName(String name) {
-        this.name = name;
-        root.acceptVisitor(this);
-        return foundActivity;
-    }
+  public Activity searchByName(String name) {
+    this.name = name;
+    root.acceptVisitor(this);
+    return foundActivity;
+  }
 }
