191ca0bc3b48da829e132de7debdbe95a432eb06
1601230 <1601230@uab.cat>
2022-11-21 21:52:59 +0100

    Versión 14: Nuevos comentarios en la classe actividades (el logger sigue sin ir)

--- PracticaDS/src/Activity.java
+++ PracticaDS/src/Activity.java
@@ -36,37 +36,46 @@ public abstract class Activity {
     this.listOfTags = listOfTags;
     this.father = father;
     running =  false;
-  }
+  } /*This is the constructor of the
+  Activity class, it initializes the attributes of this class with the parameters it receives.*/
 
   public String getNameActivity() {
     return Objects.requireNonNullElse(nameActivity, "null");
-  }
+  } /*This is a getter that returns the name of the activity.*/
 
   public LocalDateTime getInitialDate() {
     return initialDate;
-  }
+  } /*This is a getter that returns the date on which the activity was first started.*/
 
   public LocalDateTime getFinalDate() {
     return finalDate;
-  }
+  } /*This is a getter that returns the date on which the activity was last executed.*/
 
   public Duration getDuration() {
     return duration;
-  }
+  } /*This is a getter that returns the duration that the activity has been active.*/
 
   public List<String> getListOfTags() {
     return listOfTags;
-  }
+  } /*This is a getter that returns the duration that the activity has been active.*/
 
   public void add(Activity a) {
         //void
   }
 
-  public abstract boolean getRunning();
+  public abstract boolean getRunning(); /*This is an abstract method that is implemented by the classes that inherit
+  from the activity class (project and tasca). This method has the objective of returning if the activity is being
+  executed or not.*/
 
-  public abstract void changeTime(LocalDateTime initialDate, LocalDateTime finalDate);
+  public abstract void changeTime(LocalDateTime initialDate, LocalDateTime finalDate); /*This is an abstract method that
+  is implemented by the classes that inherit from the activity class (project and tasca). The purpose of this method is
+  to update the time of the activity.*/
 
-  public abstract void acceptVisitor(Visitor visitor);
+  public abstract void acceptVisitor(Visitor visitor); /*This is an abstract method that is implemented by the classes
+  that inherit from the activity class (project and tasca). This method is used for a visitor to call an activity so
+  that the activity can execute one of the visitor's functionalities.*/
 
-  public abstract JSONObject toJson();
+  public abstract JSONObject toJson(); /*This is an abstract method that is implemented by the classes that inherit from
+  the activity class (project and tasca). The "toJson" method is used so that before closing the program or ending the
+  execution, the data generated from the activities can be stored in JSON format.*/
 }
